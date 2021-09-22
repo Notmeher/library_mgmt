cd71de937517baffc554ca6c66da19c4b66d31f2
Meher <mehedi.nipu@northsouth.edu>
2021-09-22 21:11:41 +0600

    Initial commit

---
+++ addbookserver_page.php
@@ -0,0 +1,13 @@
+<?php
+//addserver_page.php
+include("data_class.php");
+
+
+
+$bookname=$_POST['bookname'];
+$bookdetail=$_POST['bookdetail'];
+$bookauthor=$_POST['bookauthor'];
+$bookpub=$_POST['bookpub'];
+$branch=$_POST['branch'];
+$bookprice=$_POST['bookprice'];
+$bookquantity=$_POST['bookquantity'];
